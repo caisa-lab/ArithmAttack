f206ed6a6609462d70d4f13b72e38cb580e13b2f
s28zabed <zu2377@gmail.com>
2024-05-17 18:08:31 +0200

    changes

--- script/gsm_pre_processing.py
+++ script/gsm_pre_processing.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 
 # Load the dataset
-df = pd.read_json("/home/stud/qamars1/NoiseInMath/data/train.json")
+df = pd.read_json("/home/stud/abedinz1/localDisk/nlplab/data/train.json")
 
 def extract_numeric_answer(answer):
     numeric_answer = re.search(r'#### (\d+)', answer)
