a89317ebe531ac1815001c070339204944195065
s28zabed <zu2377@gmail.com>
2024-05-27 16:44:34 +0200

    change in preprocessed data

--- script/gsm/gsm_preprocess.py
+++ script/gsm/gsm_preprocess.py
@@ -2,18 +2,18 @@ import pandas as pd
 import re
 
 # Load the dataset
-df = pd.read_json("/home/stud/abedinz1/localDisk/nlplab/data/train.json")
-
+df = pd.read_json("/home/stud/abedinz1/localDisk/nlplab/data/gsm/train.json")
 
 def extract_numeric_answer(answer):
-    numeric_answer = re.search(r"#### (\d+)", answer)
+    # Use a regex pattern that matches an optional sign (+ or -) before the digits
+    numeric_answer = re.search(r"#### ([+-]?\d+)", answer)
     if numeric_answer:
         return int(numeric_answer.group(1))
     else:
         return None
 
-
 df["numeric_answer"] = df["answer"].apply(extract_numeric_answer)
-df.to_csv("../data/train_preprocessed.csv", index=False)
+df.to_csv("/home/stud/abedinz1/localDisk/nlplab/data/gsm/train_preprocessed.csv", index=False)
+
 # Display the preprocessed dataset
 print(df.head())
