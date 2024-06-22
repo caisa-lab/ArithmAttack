26718ed8688abc4e40c7ac6e7edbfeac7373d565
s28zabed <zu2377@gmail.com>
2024-06-22 13:48:21 +0200

    Changes for accuracy of sample 50

--- script/gsm/script_accuracy.py
+++ script/gsm/script_accuracy.py
@@ -2,13 +2,23 @@ from utils import calculate_accuracy
 
 from config import access_token, DIR_PATH
 
-output_files = [
-       (f"{DIR_PATH}/data/gsm/flan/flan_gsm_response.csv","flan_gsm"),
-       (f"{DIR_PATH}/data/gsm/mistral/mistral_gsm_response.csv","mistral_gsm"),
-       (f"{DIR_PATH}/data/gsm/mistral_instruct/mistral_instruct_gsm_response.csv","mistral_instruct"),
-       (f"{DIR_PATH}/data/gsm/mistral_math/mistral_math_gsm_response.csv","mistral_math")
+prompts = [
+    "Always end the answer with {The final answer is}",
+    "Let's think step by step and always end the answer with {The final answer is}.",
+    "Always end the answer with {The final answer is} and think step by step.",
+    "Solve the following arithmetic problem step by step. Ensure to end the answer with {The final answer is}.",
+    "You are a math tutor. Solve the following arithmetic problem step by step. Always end the answer with {The final answer is}.",
+    "Think step by step through the following problem and clearly show each step of your reasoning. Ensure the final answer is clearly indicated by ending with {The final answer is}.",
+    "As a math tutor, explain your reasoning step by step for the following problem. Let's think step by step and end the answer with {The final answer is}.",
+]
 
-    ]
+# response_files = [
+#        (f"{DIR_PATH}/data/gsm/flan/flan_gsm_response.csv","flan_gsm"),
+#        (f"{DIR_PATH}/data/gsm/mistral/mistral_gsm_response.csv","mistral_gsm"),
+#        (f"{DIR_PATH}/data/gsm/mistral_instruct/mistral_instruct_gsm_response.csv","mistral_instruct"),
+#        (f"{DIR_PATH}/data/gsm/mistral_math/mistral_math_gsm_response.csv","mistral_math")
+
+#     ]
 
 # output_files = [
 #        (f"{DIR_PATH}/data/multiArith/mistral_math/mistral_math_gsm_response.csv","mistral_math"),
@@ -18,6 +28,14 @@ output_files = [
 #     ]
 
     # Call the calculate_accuracy function for each output file
-for output_file in output_files:
-	print(output_file)
-	calculate_accuracy(output_file[0],output_file[1])
+
+
+#dir_names = ['mistral','mistral_instruct','mistral_math','flan']
+dir_names = ['mistral']
+
+for dir_name in dir_names:
+    for i, _ in enumerate(prompts):
+        response_file_path = f"{DIR_PATH}/data/gsm/{dir_name}/{dir_name}_gsm_prompt_{i}.csv"
+        print("response_file_path: ",response_file_path)
+        print("dir_name: ",dir_name)
+        calculate_accuracy(response_file_path, dir_name)
--- script/gsm/script_run.py
+++ script/gsm/script_run.py
@@ -5,17 +5,22 @@ import os
 
 # List of scripts to run sequentially
 scripts = [
-    f"{DIR_PATH}script/gsm/mistral_gsm.py",
-    f"{DIR_PATH}script/gsm/mistral_instruct_gsm.py",
-    f"{DIR_PATH}script/gsm/mistral_math_gsm.py",
-    f"{DIR_PATH}script/gsm/flan_gsm.py"
+    f"{DIR_PATH}/script/gsm/mistral_gsm.py",
+    # f"{DIR_PATH}script/gsm/mistral_instruct_gsm.py",
+    # f"{DIR_PATH}script/gsm/mistral_math_gsm.py",
+    # f"{DIR_PATH}script/gsm/flan_gsm.py"
 ]
 
 prompts = [
     "Always end the answer with {The final answer is}",
     "Let's think step by step and always end the answer with {The final answer is}.",
     "Always end the answer with {The final answer is} and think step by step.",
+    "Solve the following arithmetic problem step by step. Ensure to end the answer with {The final answer is}.",
+    "You are a math tutor. Solve the following arithmetic problem step by step. Always end the answer with {The final answer is}.",
+    "Think step by step through the following problem and clearly show each step of your reasoning. Ensure the final answer is clearly indicated by ending with {The final answer is}.",
+    "As a math tutor, explain your reasoning step by step for the following problem. Let's think step by step and end the answer with {The final answer is}.",
 ]
+
 
 # Function to create a command with prompts
 def create_command(script, prompt):
@@ -26,17 +31,18 @@ def create_command(script, prompt):
     return command
 
 # Make sure this is in sync with the script order
-dir_name = ['mistral','mistral_instruct','mistral_math','flan']
+#dir_name = ['mistral','mistral_instruct','mistral_math','flan']
+dir_name = ['mistral']
 
 # Run each script sequentially with the prompts
-for script in scripts:
+for script_pointer, script in enumerate(scripts):
     for i, prompt in enumerate(prompts):
         sc_name = os.path.splitext(os.path.basename(script))[0]
         print(sc_name)
         print("$$")
         print("Running:", script)
         # Create the command with the current script and all prompts
-        cmd_line_args = f"{DIR_PATH}data/gsm/sample_test_preprocessed.csv {DIR_PATH}data/gsm/{dir_name[i]}/{sc_name}_prompt_{i} {prompt}"
+        cmd_line_args = f"{DIR_PATH}/data/gsm/sample_test_preprocessed.csv {DIR_PATH}/data/gsm/{dir_name[script_pointer]}/{sc_name}_prompt_{i}.csv {prompt}"
         command = create_command(script,cmd_line_args)
         # Execute the command
         subprocess.run(command, shell=False)
