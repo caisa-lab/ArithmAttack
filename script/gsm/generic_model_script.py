c8bbaeeb2032c885eedb8ee6092e1833a07e580b
s28zabed <zu2377@gmail.com>
2024-10-05 15:19:38 +0200

    Change for generic model script

---
+++ script/gsm/generic_model_script.py
@@ -0,0 +1,114 @@
+import torch
+import sys
+
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    BitsAndBytesConfig,
+)
+import csv
+
+from config import access_token, DIR_PATH
+
+from tqdm import tqdm
+
+from utils import (
+    get_questions_and_answer_from_multiArith_dataset,
+    get_questions_and_answer_from_dataset)
+
+
+
+args = sys.argv[1].split()
+
+input_file = args[0]
+output_file = args[1]
+model_name = args[2]
+prompt = ' '.join(args[3:])
+
+access_token = access_token
+tokenizer = AutoTokenizer.from_pretrained(model_name, token=access_token)
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    token=access_token,
+    device_map={"": 0},
+    quantization_config=bnb_config,
+)
+
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+
+
+print('Input file ', input_file)
+print('Output file ', output_file)
+print('Prompt ', prompt)
+
+# csv_file = f"{DIR_PATH}/data/multiArith/test_preprocessed.csv"
+# csv_file = f"{DIR_PATH}data/gsm/sample_test_preprocessed.csv"
+
+#questions, ground_truths = get_questions_and_answer_from_multiArith_dataset(input_file)
+questions, ground_truths = get_questions_and_answer_from_dataset(input_file)
+
+
+# output_file = f"{DIR_PATH}/data/multiArith/mistral_instruct/mistral_instruct_multiArith_response.csv"
+# output_file = f"{DIR_PATH}/data/gsm/mistral_instruct/mistral_instruct_gsm_response.csv"
+# output_file = f"{DIR_PATH}/data/gsm/mistral_instruct/mistral_instruct_gsm_response_hugginface.csv"
+
+# Command line arguments for prompts
+# if len(sys.argv) > 1:
+#     prompt = sys.argv[1:]  # Assume each argument is a separate prompt
+
+counter = 0
+with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
+    fieldnames = [
+        "Question",
+        "Answer - LLM",
+        "Answer - Ground Truth",
+    ]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    counter = 0
+
+    for question, ground_truth in tqdm(zip(questions, ground_truths), total=len(questions)):
+        messages = [
+            {
+                "role": "user",
+                "content": f"""
+                    {prompt}
+                    question:
+                    {question}
+                """
+            }
+        ]
+
+        model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+        generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+        generated_data = tokenizer.batch_decode(generated_ids)[0]
+
+        # import pprint
+        # print("##MESSAGE##")
+        # pprint.pprint(messages)
+        # print("##RESPONSE##")
+        # pprint.pprint(generated_data)
+
+        writer.writerow(
+            {
+                "Question": question,
+                "Answer - Ground Truth": ground_truth,
+                "Answer - LLM": generated_data,
+            }
+        )
+
+        counter += 1
+        if counter >= 2:
+            break
+
+print(f"Questions and answers saved to {output_file}")
--- script/gsm/script_run.py
+++ script/gsm/script_run.py
@@ -22,6 +22,16 @@ prompts = [
     #"As a math tutor, explain your reasoning step by step for the following problem. Let's think step by step and end the answer with {The final answer is}."
 ]
 
+prompt = "Think step by step through the following problem and clearly show each step of your reasoning. Ensure the final answer is clearly indicated by ending with {The final answer is}."
+
+model_names = [
+    #"microsoft/phi-2",
+    "google/gemma-7b",
+    "openai-community/gpt2",
+    "bigscience/bloom",
+    "mistralai/Mixtral-8x7B-Instruct-v0.1"
+]
+
 
 # Function to create a command with prompts
 def create_command(script, prompt):
@@ -32,19 +42,29 @@ def create_command(script, prompt):
     return command
 
 # Make sure this is in sync with the script order
-dir_name = ['llama','mistral_math','mistral_instruct',]
+#dir_name = ['llama','mistral_math','mistral_instruct',]
 
-# Run each script sequentially with the prompts
-for script_pointer, script in enumerate(scripts):
-    for i, prompt in enumerate(prompts):
-        sc_name = os.path.splitext(os.path.basename(script))[0]
-        print(sc_name)
-        print("$$")
-        print("Running:", script)
-        # Create the command with the current script and all prompts
-        cmd_line_args = f"{DIR_PATH}/data/gsm/test_preprocessed.csv {DIR_PATH}/data/gsm/{dir_name[script_pointer]}/{sc_name}_prompt_{i}.csv {prompt}"
-        command = create_command(script,cmd_line_args)
-        # Execute the command
-        subprocess.run(command, shell=False)
 
-print("All scripts have been executed.")
+# # Run each script sequentially with the prompts
+# for script_pointer, script in enumerate(scripts):
+#     for i, prompt in enumerate(prompts):
+#         sc_name = os.path.splitext(os.path.basename(script))[0]
+#         print(sc_name)
+#         print("$$")
+#         print("Running:", script)
+#         # Create the command with the current script and all prompts
+#         cmd_line_args = f"{DIR_PATH}/data/gsm/test_preprocessed.csv {DIR_PATH}/data/gsm/{dir_name[script_pointer]}/{sc_name}_prompt_{i}.csv {prompt}"
+#         command = create_command(script,cmd_line_args)
+#         # Execute the command
+#         subprocess.run(command, shell=False)
+
+# print("All scripts have been executed.")
+
+
+for model in model_names:
+    model_name = model.split('/')[1]
+    cmd_line_args = f"{DIR_PATH}/data/gsm/test_preprocessed.csv {DIR_PATH}/data/gsm/{model_name}/{model_name}.csv {model} {prompt}"
+    script = "generic_model_script.py"
+    command = create_command(script,cmd_line_args)
+    # Execute the command
+    subprocess.run(command, shell=False)
